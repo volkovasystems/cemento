c0263d113fb8c2fe69f32b663f4302f2686889f5
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-19 19:20:49 +0800

    Save changes.

--- test.module.js
+++ test.module.js
@@ -4,9 +4,13 @@ const cemento = require( "./cemento.js" );
 let test = { };
 let entity = cemento( { "name": "simple" }, test );
 let descriptor = Object.getOwnPropertyDescriptor( entity, "name" );
+
 assert.equal( entity[ "name" ], "simple", "should have value 'simple'" );
+
 assert.equal( descriptor[ "writable" ], false, "should be false" );
+
 assert.equal( descriptor[ "configurable" ], false, "should be false" );
+
 assert.equal( descriptor[ "enumerable" ], false, "should be false" );
 
 console.log( "ok" );
